3d967b35f88ddbd2cddd8af91bf3f00bfc58ddad
Krecik900 <kacperkrefft12@gmail.com>
2024-03-20 08:50:47 +0100

    dasda

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 
+
 from .models import Question
+
+
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 
 
@@ -12,8 +17,13 @@ def counter(request):
     return HttpResponse(template.render(context, request))
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def results(request, question_id):
@@ -25,15 +35,6 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
