0c2df433cee2d2e2761f436aeb993499269e5f6d
Krecik900 <kacperkrefft12@gmail.com>
2024-03-20 07:52:46 +0100

    commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,3 +13,16 @@ def counter(request):
     template = loader.get_template('counter.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
