7da7a154e493fca2cf4fad707958a3c127d065f8
Krecik900 <kacperkrefft12@gmail.com>
2024-03-20 08:03:18 +0100

    cos6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,10 +4,7 @@ from django.template import loader
 from .models import Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = "<br>".join(["<p>" + q.question_text + "</p>" for q in latest_question_list])
-    return HttpResponse(output)
+
 
 def counter(request):
     template = loader.get_template('counter.html')
